ec915753fac3a66607ab5408c4d0497b4f7173d5
moghit-eou <moghit.elouaad1@gmail.com>
2026-02-08 22:00:30 +0100

    Implement command execution flow

--- internal/control/handler.go
+++ internal/control/handler.go
@@ -1,1 +1,26 @@
 package control
+
+import (
+	"node-agent/internal/execution"
+	"fmt"
+)
+
+func HandleJob(commandPayLoad string) string {
+
+	if commandPayLoad == "" {
+		return "Error : Empty command"
+	}
+
+	result , err := execution.RunCommand(commandPayLoad)
+
+	if err != nil {
+		return fmt.Sprintf("Something went wrong: %v" , err )
+
+	}
+
+	response := fmt.Sprintf("Stdout : %s \nStderr: %s\nExistcode: %d",
+				result.Stdout , result.Stderr , result.ExitCode)
+
+	return response 
+
+}
