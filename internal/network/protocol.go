1120f72d7425a8ac2a3c2e94796e6568a845f014
moghit-eou <moghit.elouaad1@gmail.com>
2026-03-18 23:08:00 +0000

    feat(CI/CD): adding password leakage check

--- internal/network/protocol.go
+++ internal/network/protocol.go
@@ -5,15 +5,17 @@ type Request struct {
 	Payload string `json:"_payload_"`
 }
 
-/* request.json example
-{
-    "_Type_": "job",
-    "_payload_": "ls"
-}
+/*
+	request.json example
 
+	{
+	    "_Type_": "job",
+	    "_payload_": "ls"
+	}
 */
 type Response struct {
-	Status  string `json:"status_"`
+	Status string `json:"status_"`
+
 	Message string `json:"message_"` // json name is used during testing calls
 	// Message in GO , message in json
 	// it's a mapping layer
